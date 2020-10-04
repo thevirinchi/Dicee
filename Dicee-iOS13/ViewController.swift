fd604de70de0346195824e93d983d834ddd5d435
z3t4z00k <zetazook@gmail.com>
2020-10-04 20:25:58 +0530

    Loading random images on button click

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,16 +12,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
+        
     @IBAction func roleButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceSix")
+        
+        let dices = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = dices[Int.random(in: 0...5)]
+        diceImageView2.image = dices[Int.random(in: 0...5)]
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        diceImageView1.image = #imageLiteral(resourceName: "DiceOne")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceSix")
     }
 
 
